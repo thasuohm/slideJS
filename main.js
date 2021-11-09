ad5409a965aae6d5d8e569099f770fd2eaf5eb25
thasuohm <thasuohm@gmail.com>
2021-11-09 15:18:51 +0700

    animate slide

--- main.js
+++ main.js
@@ -1,45 +1,60 @@
-var current = 1;
-
 const move = (n) => {
-  if (current + n <= 5 && current + n >= 1) {
-    current += n;
-    cal();
-    return;
-  }
-  if (current + n > 5) {
-    current = 1;
-    cal();
-    return;
-  }
-  if (current + n < 1) {
-    current = 5;
-    cal();
-    return;
-  }
-};
+  var x = document.getElementById("item1");
+  y = x.style.marginLeft;
 
-const cal = () => {
-  for (var i = 1; i <= 5; i++) {
-    if (i === current) {
-      continue;
+  if (n === 1) {
+    if (y === "-4000px") {
+      x.style.marginLeft = "0px";
+    } else {
+      x.style.marginLeft = y.slice(0, y.length - 2) - 1000 + "px";
     }
-    document.getElementById("item" + i).style.display = "none";
-    document.getElementById("dot" + i).classList.remove("currentClick");
   }
-  document.getElementById("dot" + current).classList.add("currentClick");
-  document.getElementById("item" + current).style.display = "block";
+  if (n === -1) {
+    if (parseInt(y.slice(0, y.length - 2)) >= 0) {
+      x.style.marginLeft = "-4000px";
+    } else {
+      x.style.marginLeft =
+        parseInt(1000 + parseInt(y.slice(0, y.length - 2))) + "px";
+    }
+  }
+
+  let cal = Math.abs(y.slice(0, y.length - 2) / +1000) + 1;
+  let ind = 0;
+  if (n > 0) {
+    if (cal >= 5) {
+      ind = 1;
+    } else {
+      ind = cal + 1;
+    }
+  } else if (n < 0) {
+    if (cal === 1) {
+      ind = 5;
+    } else {
+      ind = cal - 1;
+    }
+  }
+
+  for (let i = 1; i <= 5; i++) {
+    if (i === cal + 1) {
+      continue;
+    } else {
+      document.getElementById("dot" + cal).classList.remove("currentClick");
+    }
+  }
+
+  document.getElementById("dot" + ind).classList.add("currentClick");
 };
 
 const currentSlide = (n) => {
-  current = n;
+  var x = document.getElementById("item1");
+  x.style.marginLeft = (n - 1) * -1000 + "px";
+
   for (let i = 1; i <= 5; i++) {
-    if (i === current) {
+    if (i === n) {
       continue;
     }
     document.getElementById("dot" + i).classList.remove("currentClick");
-    document.getElementById("item" + i).style.display = "none";
   }
 
-  document.getElementById("dot" + current).classList.add("currentClick");
-  document.getElementById("item" + current).style.display = "block";
+  document.getElementById("dot" + n).classList.add("currentClick");
 };
