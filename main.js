c63f0d1a8df774d5205e7b6c97c8e1acf1280191
thasuohm <thasuohm@gmail.com>
2021-11-16 12:22:34 +0700

    increase performance and change var name

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const move = (n) => {
   var x = document.getElementById("item1");
   let y = x.style.marginLeft;
-  let domLength = document.querySelectorAll(".dot").length;
+  let dotLength = document.querySelectorAll(".dot").length;
 
   if (n === 1) {
-    if (y === (domLength - 1) * -1000 + "px") {
+    if (y === (dotLength - 1) * -1000 + "px") {
       x.style.marginLeft = "0px";
     } else {
       x.style.marginLeft = y.slice(0, y.length - 2) - 1000 + "px";
@@ -12,7 +12,7 @@ const move = (n) => {
   }
   if (n === -1) {
     if (parseInt(y.slice(0, y.length - 2)) >= 0) {
-      x.style.marginLeft = (domLength - 1) * -1000 + "px";
+      x.style.marginLeft = (dotLength - 1) * -1000 + "px";
     } else {
       x.style.marginLeft =
         parseInt(1000 + parseInt(y.slice(0, y.length - 2))) + "px";
@@ -20,6 +20,7 @@ const move = (n) => {
   }
 
   let cal = Math.abs(y.slice(0, y.length - 2) / +1000) + 1;
+
   let ind = 0;
   if (n > 0) {
     if (cal >= 5) {
@@ -35,22 +36,15 @@ const move = (n) => {
     }
   }
 
-  for (let i = 1; i <= domLength; i++) {
-    if (i === cal + 1) {
-      continue;
-    } else {
-      document.getElementById("dot" + cal).classList.remove("currentClick");
-    }
-  }
-
+  document.getElementById("dot" + cal).classList.remove("currentClick");
   document.getElementById("dot" + ind).classList.add("currentClick");
 };
 
 const currentSlide = (n) => {
   var x = document.getElementById("item1");
   x.style.marginLeft = (n - 1) * -1000 + "px";
-  let domLength = document.querySelectorAll(".dot").length;
-  for (let i = 1; i <= domLength; i++) {
+  let dotLength = document.querySelectorAll(".dot").length;
+  for (let i = 1; i <= dotLength; i++) {
     if (i === n) {
       continue;
     }
